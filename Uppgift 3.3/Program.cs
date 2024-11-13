1c569ea0d3dacca277f90b1fa3ecbdcd0d2e5f83
vist07001 <vist07001@E-PW05CGR8>
2024-11-13 11:49:15 +0100

    Add project files.

---
+++ Uppgift 3.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace uppgift3_3
+{ class Program
+    { static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar ska du hyra bilen?");
+            int timmar =int.Parse(Console.ReadLine());
+            int totalt = timmar * 80 + 950;
+            Console.WriteLine("Det kommer att kosta" +  totalt );
+        }
+    }
+}
